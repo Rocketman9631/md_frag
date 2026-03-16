4106cfe77f485acc04740de4488981eb9b319606
Rocketman9631 <gbojarunecs@gmail.com>
2026-03-16 10:48:27 +0200

    common

--- common.c
+++ common.c
@@ -1,1 +1,7 @@
 #include "common.h"
+
+struct bin {
+  size_t size;
+  struct bin *next;
+};
+void create_free_list(char *chunk_file) {}
--- common.h
+++ common.h
@@ -1,4 +1,8 @@
 #ifndef COMMON_H
 #define COMMON_H
-//
+#include <stdio.h>
+
+struct bin;
+
+void create_free_list(char *chunk_file);
 #endif
