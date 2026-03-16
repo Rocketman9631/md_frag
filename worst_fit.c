7584330e004e6b1b0853661a6dfcb5ce23f487a8
akiro <161221282+1akiro@users.noreply.github.com>
2026-03-16 23:19:49 +0200

    Implement worst_fit_test function
    Added implementation for worst_fit_test function to print test header and create free list.

--- worst_fit.c
+++ worst_fit.c
@@ -1,3 +1,9 @@
 #include "worst_fit.h"
 #include "common.h"
-void worst_fit_test(char *chunk_file, char *size_file) {}
+#include <stdio.h>
+
+void worst_fit_test(char *chunk_file, char *size_file) {
+  printf("Worst Fit Tests\n")
+  // Izveido brīvo sarakstu no chunk_file  
+  linkedlist_t *free_list = create_free_list(chunks_file);
+}
