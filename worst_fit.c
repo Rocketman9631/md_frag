c618054b13e111eb4ddb152bc64e4388d95dc939
akiro <161221282+1akiro@users.noreply.github.com>
2026-03-16 23:34:24 +0200

    Rename requests variable to size_list

--- worst_fit.c
+++ worst_fit.c
@@ -6,5 +6,5 @@ void worst_fit_test(char *chunk_file, char *size_file) {
   printf("Worst Fit Tests\n");
   // Izveido brīvo sarakstu no chunk_file  
   linkedlist_t *free_list = create_free_list(chunk_file);
-  linkedlist_t *requests = create_sizes_list(size_file);
+  linkedlist_t *size_list = create_sizes_list(size_file);
 }
