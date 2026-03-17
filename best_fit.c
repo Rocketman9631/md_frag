75de598f5463711903922f98550060f6948241fe
1akiro <linardsg05@gmail.com>
2026-03-17 17:12:14 +0200

    Best n worst fit

--- best_fit.c
+++ best_fit.c
@@ -1,3 +1,59 @@
 #include "best_fit.h"
 #include "common.h"
-void best_fit_test(char *chunk_file, char *size_file) {}
+#include <stdio.h>
+
+void *best_fit(size_t size) {
+  void *ptr = NULL;
+  bin_t *chunk_curr = CHUNKS->head;
+  bin_t *best_chunk = NULL;
+
+  while (chunk_curr != NULL) {
+    if (chunk_curr->size >= size) {
+      if (best_chunk == NULL || chunk_curr->size < best_chunk->size) {
+        best_chunk = chunk_curr;
+      }
+    }
+    chunk_curr = chunk_curr->next;
+  }
+
+  if (best_chunk != NULL) {
+    ptr = best_chunk;
+    best_chunk->size = best_chunk->size - size;
+  }
+
+  return ptr;
+}
+
+void best_fit_test(char *chunk_file, char *size_file) {
+  printf("Best Fit Tests\n");
+
+  // Izveido brīvo sarakstu no chunk_file un size_file
+  CHUNKS = create_free_list(chunk_file);
+  SIZES = create_sizes_list(size_file);
+
+  printf("======= SIZES SARAKSTS =======\n");
+  print_free_list(SIZES);
+  printf("==============================\n");
+  printf("======= CHUNK SARAKSTS =======\n");
+  print_free_list(CHUNKS);
+  printf("==============================\n");
+
+  // Apstrādā size_file katru rindu izsaucot best_fit
+  bin_t *size_curr = SIZES->head;
+  while (size_curr != NULL) {
+    printf("Rezervē %zu baitus lielu atmiņu... ", size_curr->size);
+
+    if (best_fit(size_curr->size) == NULL) {
+      printf("Neveiksmīgi\n");
+      size_curr = size_curr->next;
+      continue;
+    }
+
+    printf("Veiksmīgi\n");
+    size_curr = size_curr->next;
+  }
+
+  printf("======= CHUNK SARAKSTS =======\n");
+  print_free_list(CHUNKS);
+  printf("==============================\n");
+}
--- worst_fit.c
+++ worst_fit.c
@@ -2,9 +2,49 @@
 #include "common.h"
 #include <stdio.h>
 
+void *worst_fit(size_t size){
+    bin_t *curr = CHUNKS->head;
+    bin_t *worst = NULL;
+
+
+  while (curr != NULL) {
+    if (curr->size >= size) {
+      if (worst == NULL || curr->size > worst->size) {
+        worst = curr;
+      }
+    }
+    curr = curr->next;
+  }
+
+  if (worst == NULL) {
+    return NULL;
+  }
+
+  worst->size -= size;
+  return worst;
+}
+
 void worst_fit_test(char *chunk_file, char *size_file) {
   printf("Worst Fit Tests\n");
-  // Izveido brīvo sarakstu no chunk_file  
-  linkedlist_t *free_list = create_free_list(chunk_file);
-  linkedlist_t *size_list = create_sizes_list(size_file);
+
+  // Izveido brīvo sarakstu no chunk_file un size_file
+  CHUNKS = create_free_list(chunk_file);
+  SIZES = create_sizes_list(size_file);
+
+  // Apstrādā size_file katru rindu izsaucot worst_fit
+  bin_t *size_curr = SIZES->head;
+  while (size_curr != NULL) {
+    printf("Rezervē %zu baitus lielu atmiņu... ", size_curr->size);
+
+    if (worst_fit(size_curr->size) == NULL) {
+      fprintf(stderr,
+              "\nNeizdevās rezervēt atmiņu priekš %zu baitu lielas atmiņas",
+              size_curr->size);
+      size_curr = size_curr->next;
+      continue;
+    }
+
+    printf("Veiksmīgi\n");
+    size_curr = size_curr->next;
+  }
 }
