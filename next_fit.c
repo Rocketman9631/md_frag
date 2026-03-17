8c978a0ab7d386e0d2984a7e2e9dff67b24fff9b
Matīss Volrāts <matisvol@gmail.com>
2026-03-17 18:13:40 +0200

    Update next_fit.c
    Ielieku Next-fit. God help

--- next_fit.c
+++ next_fit.c
@@ -1,3 +1,80 @@
 #include "next_fit.h"
 #include "common.h"
-void next_fit_test(char *chunk_file, char *size_file) {}
+#include <stdio.h>
+
+/* Next Fit atceras kur notika pedeja atminas pieskirsana un sak meklesanu no tas vietas*/
+static bin_t *last = NULL;
+
+void *next_fit(size_t size) {
+    void *ptr = NULL;
+
+    
+    if (last == NULL) {
+        last = CHUNKS->head;
+    }
+
+    bin_t *chunk_curr = last;
+
+    /* Meklē no pēdējās vietas */
+    while (chunk_curr != NULL) {
+        if (chunk_curr->size >= size) {
+            ptr = chunk_curr;
+            chunk_curr->size = chunk_curr->size - size;
+            if (chunk_curr->size < 0) {
+                chunk_curr->size = 0;
+            }
+            last = chunk_curr; /* atceramies pozīciju */
+            return ptr;
+        }
+        chunk_curr = chunk_curr->next;
+    }
+
+    /* ja sasniedzam beigas, sākam no sākuma */
+    chunk_curr = CHUNKS->head;
+    while (chunk_curr != last) {
+        if (chunk_curr->size >= size) {
+            ptr = chunk_curr;
+            chunk_curr->size = chunk_curr->size - size;
+            if (chunk_curr->size < 0) {
+                chunk_curr->size = 0;
+            }
+            last = chunk_curr;
+            return ptr;
+        }
+        chunk_curr = chunk_curr->next;
+    }
+
+    return NULL;
+}
+
+void next_fit_test(char *chunk_file, char *size_file) {
+    printf("Next Fit Tests\n");
+
+    last = NULL; /* pirms testa noliekam vietaa */
+
+    CHUNKS = create_free_list(chunk_file);
+    SIZES  = create_sizes_list(size_file);
+
+    printf("======= SIZES SARAKSTS =======\n");
+    print_free_list(SIZES);
+    printf("==============================\n");
+    printf("======= CHUNK SARAKSTS =======\n");
+    print_free_list(CHUNKS);
+    printf("==============================\n");
+
+    bin_t *size_curr = SIZES->head;
+    while (size_curr != NULL) {
+        printf("Rezervē %zu baitus lielu atmiņu... ", size_curr->size);
+        if (next_fit(size_curr->size) == NULL) {
+            printf("Neveiksmīgi\n");
+            size_curr = size_curr->next;
+            continue;
+        }
+        printf("Veiksmīgi\n");
+        size_curr = size_curr->next;
+    }
+
+    printf("======= CHUNK SARAKSTS =======\n");
+    print_free_list(CHUNKS);
+    printf("==============================\n");
+}
